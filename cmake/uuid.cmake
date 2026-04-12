3d39a7d106f2166d12f895e8a722065dededaf1a
Richard <legalize@xmission.com>
2026-04-11 23:38:53 -0600

    CMAKE: Handle readline, ncurses, uuid on macOS

--- cmake/uuid.cmake
+++ cmake/uuid.cmake
@@ -3,7 +3,9 @@ if(TARGET uuid)
 endif()
 
 add_library(uuid INTERFACE)
-if(NOT WIN32)
+# On macOS, UUID is built into the system (uuid/uuid.h is in the SDK)
+# On Linux, we need libuuid from pkg-config
+if(UNIX AND NOT APPLE)
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(uuid REQUIRED IMPORTED_TARGET uuid)
     # Use uuid_INCLUDEDIR (not uuid_INCLUDE_DIRS) to work around vcpkg's
