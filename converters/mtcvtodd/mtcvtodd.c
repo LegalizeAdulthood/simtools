1461a5471eb5149f95315a797cc93160ccf2905c
Richard <legalize@xmission.com>
2026-05-04 12:53:26 -0600

    MTCVTODD: Correct usage text

--- converters/mtcvtodd/mtcvtodd.c
+++ converters/mtcvtodd/mtcvtodd.c
@@ -38,7 +38,7 @@ char *ppos, oname[256];
 FILE *ifile, *ofile;
 
 if ((argc < 2) || (argv[0] == NULL)) {
-	printf ("Usage is: verb file [file...]\n");
+	printf ("Usage is: mtcvtodd file [file...]\n");
 	exit (0);  }
 
 for (i = 1; i < argc; i++) {
