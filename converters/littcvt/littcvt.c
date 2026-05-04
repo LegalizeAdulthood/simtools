3c32a9ddc1fa035d6a81913482b91fc6d0ca6d06
Richard <legalize@xmission.com>
2026-05-04 12:53:26 -0600

    LITTCVT: Correct usage text

--- converters/littcvt/littcvt.c
+++ converters/littcvt/littcvt.c
@@ -39,7 +39,7 @@ char *ppos, oname[256];
 FILE *ifile, *ofile;
 
 if ((argc < 2) || (argv[0] == NULL)) {
-	printf ("Usage is: verb file [file...]\n");
+	printf ("Usage is: littcvt file [file...]\n");
 	exit (0);  }
 
 for (i = 1; i < argc; i++) {
