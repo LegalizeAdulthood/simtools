a8edd9e790f6168572611865262e64082b33d9c5
Richard <legalize@xmission.com>
2026-05-04 12:53:26 -0600

    INDENT: Correct usage text

--- converters/indent/indent.c
+++ converters/indent/indent.c
@@ -40,7 +40,7 @@ char *ppos, iline[256], oline[256], oname[256];
 FILE *ifile, *ofile;
 
 if ((argc < 2) || (argv[0] == NULL)) {
-	printf ("Usage is: asc file [file...]\n");
+	printf ("Usage is: indent file [file...]\n");
 	exit (0);  }
 
 for (i = 1; i < argc; i++) {
