3969130f3b54ba4bde217b17d6ca3bf90659f975
Richard <legalize@xmission.com>
2026-05-04 12:53:25 -0600

    GT7CVT: Correct usage text

--- converters/gt7cvt/gt7cvt.c
+++ converters/gt7cvt/gt7cvt.c
@@ -57,7 +57,7 @@ char *ppos, oname[256];
 FILE *ifile, *ofile;
 
 if ((argc < 2) || (argv[0] == NULL)) {
-	printf ("Usage is: verb file [file...]\n");
+	printf ("Usage is: gt7cvt file [file...]\n");
 	exit (0);  }
 
 for (i = 1; i < argc; i++) {
